d0caad09aba22d4d90b72c1f762067cee59ce316
Tashi Dolma <tadolma97@gmail.com>
2022-04-18 14:41:38 -0400

    List of three cities and font changes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Welcome to my website where I document the places that I have been and list the places where I want to go. Feel free to look around!";
 document.querySelector("body").appendChild(h2);
